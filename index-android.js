bdcf99268029cfde5413a65fbf506c4a2d82394a
Naphattharawat S <naphattharawat@gmail.com>
2023-03-28 13:37:52 +0700

    update

---
+++ index-android.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+bodyParser = require('body-parser');
+var path = require('path');
+const http = require('http');
+const request = require('request');
+const server = http.createServer(app);
+
+var cors = require('cors')
+app.use(cors())
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/:clientId/:sessionId', (req, res) => {
+  const clientId = req.params.clientId;
+  const sessionId = req.params.sessionId;
+  res.render('android', {
+    clientId,
+    sessionId
+  })
+});
+
+// app.get('/:clientId/:sessionId', (req, res) => {
+//   app.use(express.static(path.join(__dirname, 'dist')));
+//   res.sendFile(path.join(__dirname, './dist/index.html'));
+// });
+
+
+server.listen(3005, () => {
+  console.log('listening on *:3005');
+});
--- index-ios.js
+++ index-ios.js
@@ -16,11 +16,12 @@ app.use(express.static(__dirname + '/public'));
 app.get('/:clientId/:sessionId', (req, res) => {
   const clientId = req.params.clientId;
   const sessionId = req.params.sessionId;
-  res.render('index', {
+  res.render('ios', {
     clientId,
     sessionId
   })
 });
+
 // app.get('/:clientId/:sessionId', (req, res) => {
 //   app.use(express.static(path.join(__dirname, 'dist')));
 //   res.sendFile(path.join(__dirname, './dist/index.html'));
